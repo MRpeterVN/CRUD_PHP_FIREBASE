83381b035bb7079e45659b46b48d25d12c17d649
MRpeterVN <anlt.21it@vku.udn.vn>
2023-04-18 01:50:57 +0700

    Update dbcon.php

--- PHP-FIREBASE/dbcon.php
+++ PHP-FIREBASE/dbcon.php
@@ -6,10 +6,7 @@ use Kreait\Firebase\Factory;
 
 
 $factory = (new Factory)
-    ->withServiceAccount('firebase-url.json')
-    ->withDatabaseUri('https://laravel-test-625e8-default-rtdb.firebaseio.com/')
-    ->withDefaultStorageBucket('laravel-test-625e8.appspot.com');
-
+    
 $storage = $factory->createStorage();
 $database = $factory->createDatabase();
 $bucket = $storage->getBucket();
